018e35d5c735844973448b5a8394db1052062b63
dugalcedo <107317025+dugalcedo@users.noreply.github.com>
2025-04-15 18:05:58 +0200

    ny kategori

--- src/routes/test-routes/purge.js
+++ src/routes/test-routes/purge.js
@@ -9,6 +9,7 @@ const purgeRouter = Router()
 purgeRouter.delete('/purgeProducts', async (req, res) => {
     try {
         await Product.deleteMany({})
+        await putBackTony()
         res.json({ message: `All products deleted.` })
     } catch (error) {
         res.status(500)
@@ -61,6 +62,7 @@ purgeRouter.delete('/purgeAll', async (req, res) => {
         await Category.deleteMany({})
         await User.deleteMany({})
         await Order.deleteMany({})
+        await putBackTony()
         res.json({ message: `All products, categories, users, and orders deleted.` })
     } catch (error) {
         res.status(500)
@@ -71,3 +73,60 @@ purgeRouter.delete('/purgeAll', async (req, res) => {
 })
 
 export default purgeRouter
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+export async function putBackTony() {
+    try {
+        // see if tom kategori exists
+        let dvdKategori = await Category.findOne({ name: "DVD"})
+
+        // if not create
+        if (!dvdKategori) {
+            dvdKategori = await Category.create({ name: "DVD" })
+        }
+
+        // put back tony
+        await Product.create({
+            category: dvdKategori._id,
+            name: "The Sopranos Complete Series 1",
+            price: 350,
+            image: "https://cdn.hmv.com/r/w-1280/p-webp/hmv/files/0f/0fa37188-ea9f-4291-90f7-a0c65591cb78.jpg",
+            unit: "st",
+            amount: 1,
+            brand: "HBO",
+            discount: 1,
+            description: "Hela första säsongen av The Sopranos på DVD",
+            stock: 1000
+        })
+
+    } catch (_) {
+        // you didn't see nothin
+    }
+}
